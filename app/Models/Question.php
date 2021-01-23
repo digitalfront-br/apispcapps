e3f9dbd355157160a61736d741557bad0fed8ef2
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-01-23 04:54:47 -0300

    Question belongsTo relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'category_id'
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
